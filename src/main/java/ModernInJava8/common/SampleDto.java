cf5a39d070ce85d10a7e08eaed0f9235d683c5fd
kimseohae <seohae0406@gmail.com>
2020-11-25 18:06:49 +0900

    2020-11-25 java stream

---
+++ src/main/java/ModernInJava8/common/SampleDto.java
@@ -0,0 +1,31 @@
+package ModernInJava8.common;
+
+public class SampleDto {
+    private Long idx;
+    private String name;
+    private String gender;
+
+    public Long getIdx() {
+        return idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
